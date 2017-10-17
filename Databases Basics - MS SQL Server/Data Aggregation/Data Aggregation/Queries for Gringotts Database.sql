ab4e50343a9d079373e0c2b3909f8f2285e35190
Alexandra Teneva <alexteneva@gmail.com>
2017-10-17 15:04:40 +0300

    Update Queries for Gringotts Database.sql
    Format the code

--- Databases Basics - MS SQL Server/Data Aggregation/Data Aggregation/Queries for Gringotts Database.sql
+++ Databases Basics - MS SQL Server/Data Aggregation/Data Aggregation/Queries for Gringotts Database.sql
@@ -10,8 +10,8 @@ SELECT MAX(MagicWandSize) AS [Longest Magic Wand] FROM WizzardDeposits
 
 -- Problem 3
 
-SELECT DepositGroup,
-	   MAX(MagicWandSize) AS [Longest Magic Wand]
+SELECT  DepositGroup,
+	MAX(MagicWandSize) AS [Longest Magic Wand]
 FROM WizzardDeposits
 GROUP BY DepositGroup
 
@@ -24,23 +24,23 @@ ORDER BY AVG(MagicWandSize)
 
 -- Problem 5
 
-SELECT DepositGroup,
-	   SUM(DepositAmount) AS [Total Sum]
+SELECT  DepositGroup,
+	SUM(DepositAmount) AS [Total Sum]
 FROM WizzardDeposits
 GROUP BY DepositGroup
 
 -- Problem 6
 
-SELECT DepositGroup,
-	   SUM(DepositAmount) AS [Total Sum]
+SELECT  DepositGroup,
+	SUM(DepositAmount) AS [Total Sum]
 FROM WizzardDeposits
 WHERE MagicWandCreator = 'Ollivander family'
 GROUP BY DepositGroup
 
 -- Problem 7
 
-SELECT DepositGroup,
-	   SUM(DepositAmount) AS [Total Sum]
+SELECT  DepositGroup,
+	SUM(DepositAmount) AS [Total Sum]
 FROM WizzardDeposits
 WHERE MagicWandCreator = 'Ollivander family'
 GROUP BY DepositGroup
@@ -49,9 +49,9 @@ ORDER BY [Total Sum] DESC
 
 -- Problem 8
 
-SELECT DepositGroup,
-	   MagicWandCreator,
-	   MIN(DepositCharge)  
+SELECT  DepositGroup,
+	MagicWandCreator,
+	MIN(DepositCharge)  
 FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 ORDER BY MagicWandCreator, DepositGroup
@@ -60,25 +60,25 @@ ORDER BY MagicWandCreator, DepositGroup
 
 SELECT 
   CASE
-	  WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-	  WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-	  WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-	  WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-	  WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-	  WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-	  WHEN Age >= 61 THEN '[61+]'
+	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	WHEN Age >= 61 THEN '[61+]'
   END AS [Age Group],
 COUNT(*) AS [Wizard Count]
 FROM WizzardDeposits 
 GROUP BY 
   CASE
-	  WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
-	  WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
-	  WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
-	  WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
-	  WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
-	  WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
-	  WHEN Age >= 61 THEN '[61+]'
+	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	WHEN Age >= 61 THEN '[61+]'
   END
 
 -- Problem 10
@@ -102,10 +102,10 @@ ORDER BY DepositGroup DESC, IsDepositExpired
 
 SELECT SUM(wizardDep.Difference) 
 FROM (
-	SELECT FirstName,
-	       DepositAmount,
-		   LEAD(FirstName) OVER (ORDER BY Id) AS GuestWizard,
-		   LEAD(DepositAmount) OVER (ORDER BY Id) AS GuestDeposit,
-		   DepositAmount - LEAD(DepositAmount) OVER (ORDER BY Id) AS Difference
+	SELECT  FirstName,
+	        DepositAmount,
+		LEAD(FirstName) OVER (ORDER BY Id) AS GuestWizard,
+		LEAD(DepositAmount) OVER (ORDER BY Id) AS GuestDeposit,
+		DepositAmount - LEAD(DepositAmount) OVER (ORDER BY Id) AS Difference
 	FROM WizzardDeposits
 ) AS wizardDep
