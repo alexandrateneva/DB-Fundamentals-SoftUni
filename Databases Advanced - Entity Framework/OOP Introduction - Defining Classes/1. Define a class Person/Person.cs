08207705b9e516e669bdc453fb8a81c57245992b
Alexandra Teneva <alexteneva@gmail.com>
2017-10-26 11:59:34 +0300

    Add OOP Introduction - Defining Classes exercises

---
+++ Databases Advanced - Entity Framework/OOP Introduction - Defining Classes/1. Define a class Person/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace _1.Define_a_class_Person
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/OOP Introduction - Defining Classes/1. Define a class Person/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _1.Define_a_class_Person
+{
+    using System;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type personType = typeof(Person);
+            PropertyInfo[] properties = personType.GetProperties
+                (BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(properties.Length);
+
+            Person firstPerson = new Person("Pesho", 20);
+            Person secondPerson = new Person("Gosho", 17);
+            Person thirdPerson = new Person("Stamat", 43);
+        }
+    }
+}
